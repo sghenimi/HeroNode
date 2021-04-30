d0a33e4b1b72a00a427ace8667940186a7e1dc75
sghenimi <soufiane.ghenimi@gmail.com>
2021-04-30 13:04:35 +0200

    test

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200);
-    res.end('Hello Hero Node')
-}).listen()
+var msg = 'Hello Node';
+console.log(msg);
+console.log(msg.split(''));
+mes = msg.split('')
+mes.forEach((data) => console.log(data.toUpperCase()));
+console.log(msg.length);
+let i = 0;
+setInterval(() => {
+    console.log('iteration : ', i);
+    i++
+}, 5000)
